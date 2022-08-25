d01c3c08575e7812284bcf55d6fbcaa955e9dd96
Rupesh2403 <112059967+Rupesh2403@users.noreply.github.com>
2022-08-25 16:09:05 +0530

    The Information Completed
    Third Commit

---
+++ MyName/src/project/Practice.java
@@ -0,0 +1,19 @@
+package project;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("My Name is Rupesh Admane");
+
+		System.out.println("I leaved at umred");
+		
+		System.out.println("I have completed a graduation");
+		
+		System.out.println("My Mobile No Is 9552753388");
+		
+		System.out.println("My Adhar Id is 537267103291");
+		
+	}
+
+}
